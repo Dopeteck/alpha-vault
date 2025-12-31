688617e3437ef8cdc3eaa0ab4b805690b86d2b0c
Dopeteck <165482307+Dopeteck@users.noreply.github.com>
2025-12-31 03:48:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -364,6 +364,13 @@ startStudyTimer: function(courseId, source, type) {
         }, 1000);
     },
 
+    // --- HELPERS ---
+    getYouTubeID: function(url) {
+        const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
+        const match = url.match(regExp);
+        return (match && match[2].length === 11) ? match[2] : null;
+    },
+
     completeCourse: function(courseId, btn) {
         if (this.state.completed.includes(courseId)) return;
         this.state.completed.push(courseId);
